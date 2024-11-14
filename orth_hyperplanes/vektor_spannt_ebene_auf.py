bb023dcc34426748986618838949651ef8517190
Pauleano <geigerpaul944@gmail.com>
2024-11-14 23:51:08 +0100

    removed zones approach

--- orth_hyperplanes/vektor_spannt_ebene_auf.py
+++ orth_hyperplanes/vektor_spannt_ebene_auf.py
@@ -131,60 +131,6 @@ def multivar_planes(multivar_data,emb_dim,emb_delay):
     return list_of_words,abfolge_words,haeuf_words
 
 
-
-#erweiterung methode damit nicht mehr nur links;rechts/+;- geprüft werden kann sondern [0,pi] aufgeteilt in zonecount viele zonen
-#cos(pi)=-1-->1.zone, cos(0)=1-->letzte zone 
-def multivar_planes_zones(multivar_data,emb_dim,emb_delay,zonecount):
-    #for i in range(anzahl der fenster):
-        
-    words=[]
-    for i in range(multivar_data.shape[1]-emb_dim*emb_delay):
-        
-        #relevante daten aus reihe lesen |data in ausschnitt|=emb_dim+1
-        relev_ausschnitt=np.zeros((multivar_data.shape[0],emb_dim+1))
-        for j in range(emb_dim+1):
-            relev_ausschnitt[:,j]=multivar_data[:,i+j*emb_delay]
-        
-        #struktur:[x1-x0,x2-x1,x3-x2,x4-x3|x5-x1,x5-x2,x5-x3,x5-x4]--> beispiel für ausschnitt (x0,...,x5)
-        #neuer schritt muss mit allen vorherigen ebenen verglichen werden um exakte position zu wissen
-        relev_diff=np.zeros((multivar_data.shape[0],(relev_ausschnitt.shape[1]-2)*2))
-        #für jeden neuen punkt differenzen zu bisherigen ebenenpunkten berechnen
-        for j in range(1,relev_ausschnitt.shape[1]-1):
-            relev_diff[:,j-1]=relev_ausschnitt[:,j]-relev_ausschnitt[:,j-1]
-            relev_diff[:,(emb_dim-1)+(j-1)]=relev_ausschnitt[:,emb_dim]-relev_ausschnitt[:,j]
-        #in for schleife matrizen (bisherige ebenen) erstellen und mit vektor (neue schritt) multiplizieren
-        #ergebnis=vektor... soll in ergebnismatrix als zeile übertragen werden
-        #plane_matrix=np.zeros((multivar_data.shape[0],emb_dim-1))
-        ergebnis_matrix=np.zeros((1,emb_dim-1))
-
-        #formel für indexe der differenzen für vergleich: gaus(k-1)=index ebene in rel_diff
-        for k in range(emb_dim-1):
-            #plane=relev_diff[:,k].T
-            #vector=relev_diff[:,emb_dim-1+k]
-            ergebnis_matrix[0,k]=relev_diff[:,k] @ relev_diff[:,emb_dim-1+k]
-            #cos-winkelbestimmung (szenario nullteilung bei differenz=0)
-            ergebnis_matrix[0,k]=ergebnis_matrix[0,k]/((abs(np.dot(relev_diff[:,k],relev_diff[:,k]))**0.5)*(abs(np.dot(relev_diff[:,emb_dim-1+k],relev_diff[:,emb_dim-1+k]))**0.5)) 
-        
-        #mithilfe ergebnismatrix wort bestimmen und in wörterliste packen
-        #bei links rechts entscheidung nur vorzeichen relevant
-        #bei fallunterscheidung
-        wordpart=[]
-        for j in range(ergebnis_matrix.shape[1]):
-            #nur bis inklusive dem eintrag auf der Diagonalen gehen
-            if ergebnis_matrix[0,j]==-1:
-                wordpart.append('1')
-            else:
-                zone=math.ceil((ergebnis_matrix[0,j]+1)*zonecount/2)
-                wordpart.append(str(zone))    
-        words.append(''.join(wordpart))
-                
-    #output mithilf np.unique bekommen (input zwar liste nicht array aber unique macht als erstes array draus)
-    [list_of_words,abfolge_words,haeuf_words]=np.unique(words,return_inverse=True, return_counts=True)
-
-    return list_of_words,abfolge_words,haeuf_words
-
-
-
 def shannon(haeufigk,log_basis):
     entropy=0
     #bestimmung wahrscheinlichkeiten
