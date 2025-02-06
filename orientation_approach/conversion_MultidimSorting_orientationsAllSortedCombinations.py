a62696c9fe46f7cb4818f90d7eb52944d8aebb21
Pauleano <geigerpaul944@gmail.com>
2025-02-06 18:20:57 +0100

    Create conversion_MultidimSorting_orientationsAllSortedCombinations.py

---
+++ orientation_approach/conversion_MultidimSorting_orientationsAllSortedCombinations.py
@@ -0,0 +1,8 @@
+#functions to convert from output orientations_all_sorted_combinations to multidimensional sorting matrix
+import numpy as 
+import itertools as iter
+
+def matrix_from_symbol(string)
+  
+  
+  return
