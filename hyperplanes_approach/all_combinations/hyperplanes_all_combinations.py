0b6b99cdd282fb27b8b0f9e5057fb49fdac678bf
Pauleano <geigerpaul944@gmail.com>
2025-02-28 22:57:55 +0100

    Update and rename hyperplanes_all_sorted_combinations.py to hyperplanes_all_combinations.py

--- hyperplanes_approach/all_combinations/hyperplanes_all_combinations.py
+++ hyperplanes_approach/all_combinations/hyperplanes_all_combinations.py
@@ -1,7 +1,7 @@
 import numpy as np
 import itertools as iter
 
-def hyperplanes_all_sorted_combinations(multivar_data,emb_dim,emb_delay):
+def hyperplanes_all_combinations(multivar_data,emb_dim,emb_delay):
     #
     emb_dim+=1
     # einschränkung: max fensterlänge=10 (0,1,2,3,4,5,6,7,8,9)
